3f8bf6dfc9e6a31912fefebb7197b946938977e1
Charles Ellis <hamled@hamled.name>
2018-11-20 23:10:42 -0800

    CORS tests include likely origins
    * `host` - Default host in tests (unconfigured it's `www.example.com`)
    * `localhost:3000` - Default Rails/create-react-app host
    * `null` - Origin value for requests made by `file://` sources

--- test/integration/cors_headers_test.rb
+++ test/integration/cors_headers_test.rb
@@ -2,14 +2,16 @@ require 'test_helper'
 
 class CorsHeadersTest < ActionDispatch::IntegrationTest
   test "responses include CORS headers" do
-    get '/', headers: {origin: host}
+    [host, 'http://localhost:3000', 'null'].each do |host|
+      get '/', headers: {origin: host}
 
-    assert_includes response.headers, 'Access-Control-Allow-Origin'
-    assert_equal response.headers['Access-Control-Allow-Origin'], host
+      assert_includes response.headers, 'Access-Control-Allow-Origin'
+      assert_equal response.headers['Access-Control-Allow-Origin'], host
 
-    assert_includes response.headers, 'Access-Control-Allow-Methods'
-    %w(GET POST PUT OPTIONS).each do |method|
-      assert_includes response.headers['Access-Control-Allow-Methods'], method
+      assert_includes response.headers, 'Access-Control-Allow-Methods'
+      %w(GET POST PUT OPTIONS).each do |method|
+        assert_includes response.headers['Access-Control-Allow-Methods'], method
+      end
     end
   end
 end
